db492a8ad932bd3e955dd5d62780fe9dcf4f3926
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-02 18:09:10 -0500

    added message schema

---
+++ models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+	title: { type: String, required: [true, "Please enter a title"] },
+	date: { type: Date, default: Date.now },
+	message: { type: String, required: [true, "Please enter a message"] },
+	user: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+messageSchema.virtual("date_formatted").get(function() {
+    return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+})
+
+module.exports = mongoose.model("Message", messageSchema)
