c424ae14995867eabc225972985975f31c17f71d
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-02 19:08:28 -0500

    update and include mongodb key

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const mongoose = require('mongoose')
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Schema = mongoose.schema;
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+const mongoDB = process.env.MONGODB_KEY
+mongoose.connect(mongoDB)
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
