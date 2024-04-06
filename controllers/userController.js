e8e719403f91d53a48e7d386bf41e8bf35528b61
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-05 20:20:24 -0500

    create sign-up form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,16 @@
+const { body, validationResult } = require("express-validator");
+const Message = require("../models/message");
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+
 //sign-up form to create user
+
+exports.user_create_get = asyncHandler(async(req, res, next) => {
+    res.render("sign-up", {
+        title: "User form"
+    })
+})
+
+exports.user_create_post = asyncHandler(async(req, res, next) => {
+    
+})
