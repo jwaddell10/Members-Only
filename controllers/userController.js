12f658fa06ba68348ebae86daa2753ae52d9147d
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-07 19:46:09 -0500

    question

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,30 +6,12 @@ const asyncHandler = require("express-async-handler");
 //sign-up form to create user
 
 exports.user_create_get = asyncHandler(async (req, res, next) => {
-<<<<<<< HEAD
     console.log('is this running?')
-=======
->>>>>>> db305628f99b7723c36ec0d2aec0c7537138b336
 	res.render("sign-up", {
 		title: "Sign-Up Form",
 	});
 });
 
-<<<<<<< HEAD
-// exports.user_create_post = asyncHandler(async (req, res, next) => {
-// 	//need values from the form, need to sanitize them
-// 	// body("username") "password" "password"
-// 	console.log(req.body, "this is reqbody");
-// 	try {
-// 		const user = new User({
-// 			username: req.body.username,
-// 			password: req.body.password,
-// 		});
-// 	} catch {
-// 		console.log("this is caught");
-// 	}
-// });
-=======
 exports.user_create_post = asyncHandler(async (req, res, next) => {
 	//need values from the form, need to sanitize them
 	// body("username") "password" "password"
@@ -43,4 +25,3 @@ exports.user_create_post = asyncHandler(async (req, res, next) => {
 		console.log("this is caught");
 	}
 });
->>>>>>> db305628f99b7723c36ec0d2aec0c7537138b336
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,6 @@ const user_controller = require('../controllers/userController')
 
 router.get('/sign-up', user_controller.user_create_get)
 
-<<<<<<< HEAD
-// router.post('/sign-up', user_controller.user_create_post)
-=======
 router.post('/sign-up', user_controller.user_create_post)
->>>>>>> db305628f99b7723c36ec0d2aec0c7537138b336
 
 module.exports = router;
