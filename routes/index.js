744d9fee37776ab7ebc473b1961d33a9c5d57363
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-08 09:40:00 -0500

    let user click from sign-up form to homepage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
+const user_controller = require('../controllers/userController')
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
 	res.render("index", { title: "ChatterZone" });
 });
+
+router.get('/sign-up', user_controller.user_create_get)
 
 module.exports = router;
