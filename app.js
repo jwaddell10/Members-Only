72983bec16793a5027fbfd5cc3c33251a6a124c9
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-21 12:44:08 -0500

    update

--- app.js
+++ app.js
@@ -91,11 +91,4 @@ passport.deserializeUser(async (id, done) => {
 	}
 });
 
-// Use PORT provided in environment or default to 3000
-const port = process.env.PORT || 3000;
-
-// Listen on `port` and 0.0.0.0
-app.listen(port, "0.0.0.0", function () {
-});
-
 module.exports = app;
