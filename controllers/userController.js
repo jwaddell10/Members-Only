1842e398c17218ad1a7cf946721d618160e45ffa
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-21 10:55:46 -0500

    update bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,25 +18,30 @@ exports.profilePost = asyncHandler(async (req, res, next) => {
 		const userEnteredClubPassword = req.body.clubpassword;
 		const userEnteredAdminPassword = req.body.adminpassword;
 		const correctClubPassword = process.env.club_password;
-		const correctAdminPassowrd = process.env.admin_password;
+		const correctAdminPassword = process.env.admin_password;
 
 		if (userEnteredClubPassword === correctClubPassword) {
 			loggedInUser.status = true;
 			await User.updateOne({ _id: loggedInUser._id }, { status: true });
-			if (userEnteredAdminPassword === correctAdminPassowrd) {
+
+			// Check if the admin password is correct
+			if (userEnteredAdminPassword === correctAdminPassword) {
 				loggedInUser.admin = true;
 				await User.updateOne(
 					{ _id: loggedInUser._id },
 					{ admin: true }
 				);
 			}
-			return res.redirect("/");
-		}
 
-		res.render("profile", {
-			title: "Profile",
-			errorMessage: "Incorrect club password. Please try again.",
-		});
+			// Redirect to the appropriate page
+			return res.redirect("/");
+		} else {
+			// If club password is incorrect, show an error message
+			res.render("profile", {
+				title: "Profile",
+				errorMessage: "Incorrect club password. Please try again.",
+			});
+		}
 	} catch (error) {
 		next(error);
 	}
